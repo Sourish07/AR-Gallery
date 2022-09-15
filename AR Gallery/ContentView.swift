ca707d6842c5810a0c8a145919b82e49646d4bf3
Sourish Kundu <sourishkundu07@gmail.com>
2022-09-14 19:48:33 -0500

    Refactored nav bar's image picker into separate struct

--- AR Gallery/ContentView.swift
+++ AR Gallery/ContentView.swift
@@ -10,53 +10,99 @@ import RealityKit
 import PhotosUI
 
 struct ContentView : View {
-    @State var selectedItems: [PhotosPickerItem] = []
-    @State var selectedPhotosData: [Data] = []
+    
+    @State private var selectedImageForPlacement: ModelEntity?
 
     var body: some View {
         ZStack(alignment: .bottom) {
             ARViewContainer().edgesIgnoringSafeArea(.all)
-            HStack {
-                PhotosPicker(
-                    selection: $selectedItems,
-                    matching: .images
-                ) {
-                    NavBarIcon(image: Image(systemName: "photo.fill"))
-                }
-                .onChange(of: selectedItems) { newItems in
-                    selectedPhotosData.removeAll()
-                    for newItem in newItems {
-                        print("LOADING IMAGE")
-                        Task {
-                            if let data = try? await newItem.loadTransferable(type: Data.self) {
-                                selectedPhotosData.append(data)
-                            }
-                        }
-                    }
-                }
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack {
-                        ForEach(selectedPhotosData, id: \.self) { photoData in
-                            if let image = UIImage(data: photoData) {
-                                NavBarIcon(image: Image(uiImage: image))
-                            }
-                        }
-                    }
+            NavBarPhotos(selectedImageForPlacement: $selectedImageForPlacement)
+        }
+    }
+}
 
+struct NavBarPhotos: View {
+    @State private var selectedItems: [PhotosPickerItem] = []
+    @State private var selectedPhotosData: [Data] = []
+    
+    @Binding var selectedImageForPlacement: ModelEntity?
+    
+    var body: some View {
+        HStack {
+            PhotosPicker(
+                selection: $selectedItems,
+                matching: .images
+            ) {
+                NavBarIcon(image: Image(systemName: "photo.fill"))
+            }
+            .onChange(of: selectedItems) { newItems in
+                selectedPhotosData.removeAll()
+                for newItem in newItems {
+                    print("LOADING IMAGE")
+                    Task {
+                        if let data = try? await newItem.loadTransferable(type: Data.self) {
+                            selectedPhotosData.append(data)
+                        }
+                    }
                 }
             }
-            .padding(20)
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(selectedPhotosData, id: \.self) { photoData in
+                        if let image = UIImage(data: photoData) {
+                            NavBarPictureButton(image: image, selectedImageForPlacement: $selectedImageForPlacement)
+                        }
+                    }
+                }
+            }
         }
+        .padding(20)
     }
 }
 
 struct NavBarIcon: View {
     var image: Image
-
+    
     var body: some View {
         image.resizable()
             .scaledToFit()
             .frame(height: 40)
+    }
+}
+
+struct NavBarPictureButton: View {
+    var image: UIImage
+    
+    @Binding var selectedImageForPlacement: ModelEntity?
+    
+    var body: some View {
+        Button(action: {
+            let cgImage = image.cgImage
+            let textureResource = try! TextureResource.generate(from: cgImage!, options: TextureResource.CreateOptions(semantic: .raw))
+            let imgTexture = MaterialParameters.Texture.init(textureResource)
+            
+            let longerLength: Float = 0.5
+            var planeHeight: Float? = nil
+            var planeWidth: Float? = nil
+            if imgTexture.resource.height > imgTexture.resource.width {
+                planeHeight = longerLength
+                planeWidth = Float(imgTexture.resource.width) / (Float(imgTexture.resource.height) / longerLength)
+            } else {
+                planeWidth = longerLength
+                planeHeight = Float(imgTexture.resource.height) / (Float(imgTexture.resource.width) / longerLength)
+            }
+            
+            var material = SimpleMaterial()
+            material.color = .init(tint: .white, texture: imgTexture)
+            material.roughness = 1
+            material.metallic = 1
+            
+            let mesh = MeshResource.generatePlane(width: planeWidth!, depth: planeHeight!)
+            selectedImageForPlacement = ModelEntity(mesh: mesh, materials: [material])
+            
+        }, label: {
+            NavBarIcon(image: Image(uiImage: image))
+        })
     }
 }
 
