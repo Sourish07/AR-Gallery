ac05c5cf808239bd11ff0af54c774a295a587e0e
Sourish Kundu <sourishkundu07@gmail.com>
2024-02-12 22:28:01 -0800

    0. Added comments to default code

--- AR-Gallery/ContentView.swift
+++ AR-Gallery/ContentView.swift
@@ -20,17 +20,22 @@ struct ARViewContainer: UIViewRepresentable {
         
         let arView = ARView(frame: .zero)
 
-        // Create a cube model
+        // 1. Create a cube model
+        
+        // 1a. Create a cube mesh
         let mesh = MeshResource.generateBox(size: 0.1, cornerRadius: 0.005)
+        // 1b. Create material
         let material = SimpleMaterial(color: .gray, roughness: 0.15, isMetallic: true)
+        // 1c. Create a model entity with the mesh & material
         let model = ModelEntity(mesh: mesh, materials: [material])
-        model.transform.translation.y = 0.05
-
-        // Create horizontal plane anchor for the content
+        model.transform.translation.y = 0.05 // Translating up by 0.05 so cube sits flush on plane
+        
+        // 2. Create horizontal plane anchor for the content
+        // Looking for a horizontal plane anywhere (e.g. ceiling, floor, table, seat, etc.)
         let anchor = AnchorEntity(.plane(.horizontal, classification: .any, minimumBounds: SIMD2<Float>(0.2, 0.2)))
-        anchor.children.append(model)
-
-        // Add the horizontal plane anchor to the scene
+        anchor.children.append(model) // Attaching the virtual model to the anchor point in the real world
+        
+        // 3. Add the horizontal plane anchor to the scene
         arView.scene.anchors.append(anchor)
 
         return arView
